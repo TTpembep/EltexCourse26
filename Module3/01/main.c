94252b15b351d6405b07654e149ea3fd541c6727
TTpembep <mega1twix@gmail.com>
2026-04-08 13:28:52 +0700

    Finished task1 module3

--- Module3/01/main.c
+++ Module3/01/main.c
@@ -1,26 +1,37 @@
 //#include <ctype.h>
-#include <stdlib.h>
-#include <stdio.h>
+#include <stdlib.h> //for atoi()
+#include <stdio.h>  //input & output
+#include <sys/types.h>  //pid_t
+#include <unistd.h> //fork()
 
 int main (int argc, char *argv[]){
-    //Разделение массива данных и запуск дочернего процесса
+    pid_t childId = fork();     //Запуск дочернего процесса
+    int i = 0;
 
+    if (childId<0){ //Проверка что процесс успешно создан
+        perror("fork failed");
+        exit(1);
+    }
+    else if(childId == 0){
+        printf("-= Child`s id: %d =-\n", getpid());
+        i++;    //Для разделения вывода. четное или нечетное
+    }
+    else{
+        printf("-= Parent`s id: %d =-\n", getpid());
+    }
 
-
-    
-    //Базовая функция для обработки входных данных
-    //Нужно продумать случаи: 42word & 42.12
-    //Работает для: word, word42, 42
-    int num;
-    for (int i=0; i < argc; i++){
-        num = atoi(argv[i]);
+    double num;
+    for (i; i < argc; i=i+2){
+        num = strtod(argv[i], NULL);
         if (num != 0){
-            printf("%d %d \n", num, num*2);
+            printf("%.2f\t%.2f \n", num, num*2);
         }
         else {printf("%s \n", argv[i]);}
     }
-
+    //^^Базовая функция для обработки входных данных
+    //В случае: 42word Вывод: 42.00 Не совсем корректно
+    //Работает для: word, word3, 16, 13.37
+    //Недостаток: Логика создания дочернего процесса смешана с основной логикой программы
     //Добавить ожидание окончания дочернего процесса?
-
     return 0;
 }
